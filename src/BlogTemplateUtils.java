eff24277e975c2e34e9a5ab6aa16d52c9c1fb3b8
user <denevell@denevell>
2013-03-02 18:46:17 +0000

    singlepages.template file in the CWD is now a requirement

--- src/BlogTemplateUtils.java
+++ src/BlogTemplateUtils.java
@@ -1,11 +1,9 @@
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.regex.Pattern;
 
 import utils.FileUtils;
 
@@ -18,36 +16,12 @@ import entities.Page;
 
 
 public class BlogTemplateUtils {
-	
-
 
 	private static final String SINGLEPAGE_TEMPLATE_LOCATION = "singlepage.template";
-	private static final String sSinglePostTemplate = 
-    					"<html>" + "\n" +
-    							"<head>" +" \n" +
-	    							"<title>" +" \n" +
-				    					"{{#attr}}{{title}}{{/attr}}" + "\n" +
-	    							"</title" +" \n" +
-    							"</head>" +" \n" +
-	    						"<body>" +" \n" +
-		    						"<div class=\"blog-post\">" + "\n" +
-	    								"{{& post}}" + "\n" +
-		    						"<div>" + "\n" +
-		    						"<div class=\"blog-tags\">" + "\n" +
-				    					"Tags: {{#attr}}{{tags}}{{/attr}}" +"\n" +
-		    						"<div>" + "\n" +
-		    						"<div class=\"blog-date\">" + "\n" +
-				    					"Posted: {{#attr}}{{date}}{{/attr}}" +"\n" +
-		    						"<div>" + "\n" +
-    							"</body>" + "\n" +
-    					"</html>";
 
 	public static ArrayList<Page> convertBlogPostToSinglePages(ArrayList<BlogPost> bps) throws IOException {
-		//Get the template from, optionally, the dir the program is running in.
-		String pageTemplateString = "";
-		if((pageTemplateString=FileUtils.getStringFromFile(SINGLEPAGE_TEMPLATE_LOCATION))==null) {
-			pageTemplateString = sSinglePostTemplate;
-		}
+		String pageTemplateString = FileUtils.getStringFromFile(SINGLEPAGE_TEMPLATE_LOCATION);
+		if(pageTemplateString==null) throw new RuntimeException("No singlepages.template file found in CWD.");
 		ArrayList<Page> pages = new ArrayList<Page>();
 		for (BlogPost blogPost : bps) {
 		    MustacheFactory mf = new DefaultMustacheFactory();		
